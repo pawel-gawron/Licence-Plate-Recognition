cfa5810a219f834d9d7a41e38c3408b76b5ba821
pawel-gawron <gawron.pawel1999@gmail.com>
2023-05-20 23:00:30 +0200

    Upgrade network model

--- cnnLicencePlate.py
+++ cnnLicencePlate.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import cv2 as cv
+import cv2
 
 import os
 import tensorrt
@@ -73,9 +73,10 @@ for digit in digits:
     label = digits.index(digit)
     for img in os.listdir(path):
         img_path = os.path.join(path, img)
-        img_array = cv.imread(img_path)
-        resized = cv.resize(img_array, (IMG_WIDTH, IMG_HEIGHT), interpolation = cv.INTER_AREA)
-        gray = cv.cvtColor(resized, cv.COLOR_RGB2GRAY)        
+        img_array = cv2.imread(img_path)
+        resized = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT), interpolation = cv2.INTER_AREA)
+        gray = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)   
+        # ret3, gray = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)     
         data.append(gray)
         labels.append(label)
         if MAX_NUM is not None:
@@ -89,7 +90,7 @@ print("labels: ", labels)
 
 data = data / 255.0
 data = data.reshape(*data.shape, 1)
-labels = to_categorical(labels, num_classes=NUM_CLASSES)
+# labels = to_categorical(labels, num_classes=NUM_CLASSES)
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, shuffle=True, test_size=.3)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, shuffle=True, test_size=.3)
@@ -98,79 +99,23 @@ print("Training dataset shape: ", X_train.shape, y_train.shape)
 print("Validation dataset shape: ", X_val.shape, y_val.shape)
 print("Testing dataset shape: ", X_test.shape, y_test.shape)
 
-# (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+model = keras.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(40,32,1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-# # Normalize pixel values to be between 0 and 1
-# train_images, test_images = train_images / 255.0, test_images / 255.0
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+model.add(layers.Dense(35, activation='softmax'))
 
-# class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-#                'dog', 'frog', 'horse', 'ship', 'truck']
+model.summary()
 
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i])
-#     # The CIFAR labels happen to be arrays, 
-#     # which is why you need the extra index
-#     plt.xlabel(class_names[train_labels[i][0]])
-# plt.show()
-
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-# model.add(layers.Flatten())
-# model.add(layers.Dense(64, activation='relu'))
-# model.add(layers.Dense(10))
-
-# model.summary()
-
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
-
-# history = model.fit(X_train, y_train, epochs=10, 
-#                     validation_data=(X_val, y_val))
-
-# plt.plot(history.history['accuracy'], label='accuracy')
-# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.ylim([0.5, 1])
-# plt.legend(loc='lower right')
-
-# test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
-
-# print(test_acc)
-
-input_layer=Input(shape=(40,32,1))
-layer=Conv2D(filters=32,kernel_size=(3,3),activation="relu",padding="valid")(input_layer)
-layer=MaxPool2D(pool_size=(2,2),strides=(1,1))(layer)
-layer=BatchNormalization()(layer)
-
-layer=Conv2D(filters=16,kernel_size=(3,3),activation="relu",padding="valid")(input_layer)
-layer=MaxPool2D(pool_size=(2,2),strides=(1,1))(layer)
-layer=BatchNormalization()(layer)
-
-layer=Flatten()(layer)
-
-layer=Dense(64,activation="relu")(layer)
-layer=BatchNormalization()(layer)
-
-layer=Dense(32,activation="relu")(layer)
-output_layer=Dense(35,activation="softmax")(layer)
-model2=Model(inputs=input_layer,outputs=output_layer)
-model2.summary()
-
-model2.compile(loss='categorical_crossentropy',
-       optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-       metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 checkpoint_path = "content/training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -180,20 +125,29 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
                                                  verbose=1)
 
-history2 = model2.fit(X_train, y_train,
-                    epochs=15, batch_size=256,
+history = model.fit(X_train, y_train, epochs=20, batch_size=256,
                     validation_data=(X_val, y_val),
                     callbacks=[cp_callback])
 
-hist=history2.history
-plt.plot(hist["accuracy"],color="b",label="train_accuracy")
-plt.plot(hist["val_accuracy"],color="g",label="val_accuracy")
-plt.legend(loc="lower right")
+# Zapisz model w formacie TensorFlow Lite
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Ustaw opcje kwantyzacji
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+# Zapisz model do pliku
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
 plt.show()
 
-model2.evaluate(X_val,y_val)
+test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
 
-model2.evaluate(X_test,y_test)
-
-# Save the entire model as a SavedModel.
-model2.save('content/saved_model/my_model')
+print(test_acc)
--- plateRecognition.py
+++ plateRecognition.py
@@ -8,7 +8,16 @@ from tensorflow import keras
 
 # print(tf.version.VERSION)
 
-letters_recognition_model = tf.keras.models.load_model('content/saved_model/my_model')
+# Załaduj model TensorFlow Lite z pliku
+letters_recognition_model = tf.lite.Interpreter(model_path='/home/pawel/Documents/RISA/sem1/SW/Licence Plate Recognition/model.tflite')
+letters_recognition_model.allocate_tensors()
+
+# Pobierz indeksy wejściowych i wyjściowych tensorów
+input_details = letters_recognition_model.get_input_details()
+output_details = letters_recognition_model.get_output_details()
+input_shape = input_details[0]['shape']
+
+print("input_details: ", input_details)
 
 # Check its architecture
 # letters_recognition_model.summary()
@@ -43,20 +52,24 @@ if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(path + "/train/")
 
-    gw, gs, gw1, gs1, gw2, gs2 = (3, 1, 3, 2, 5, 1)
+    gw, gs, gw1, gs1, gw2, gs2 = (5, 2, 5, 1, 9, 17)
 
-    letterLabels = np.array(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    letterLabels = np.array(['0', '1', '2', '3', '4', '5', '6',
+                             '7', '8', '9', 'A', 'B', 'C', 'D',
+                             'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                             'L', 'M', 'N', 'P', 'Q', 'R', 'S',
+                             'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 
     for image_name in sorted(glob.glob(path + "*.jpg")):
 
         cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
 
-        cv2.createTrackbar('gw', 'image', 2, 10, empty_callback)
-        cv2.createTrackbar('gs', 'image', 0, 10, empty_callback)
-        cv2.createTrackbar('gw1', 'image', 2, 10, empty_callback)
-        cv2.createTrackbar('gs1', 'image', 0, 10, empty_callback)
-        cv2.createTrackbar('gw2', 'image', 2, 10, empty_callback)
-        cv2.createTrackbar('gs2', 'image', 0, 10, empty_callback)
+        cv2.createTrackbar('gw', 'image', 2, 20, empty_callback)
+        cv2.createTrackbar('gs', 'image', 0, 35, empty_callback)
+        cv2.createTrackbar('gw1', 'image', 2, 50, empty_callback)
+        cv2.createTrackbar('gs1', 'image', 0, 50, empty_callback)
+        cv2.createTrackbar('gw2', 'image', 2, 50, empty_callback)
+        cv2.createTrackbar('gs2', 'image', 0, 50, empty_callback)
 
         # setting position of 'G' trackbar to 100
         cv2.setTrackbarPos('gw', 'image', gw)
@@ -71,7 +84,7 @@ if __name__ == '__main__':
 
             image_ratio = image.shape[1]/image.shape[0]
 
-            dim = (800, int(800 / image_ratio))
+            dim = (900, int(900 / image_ratio))
 
             print("image_ratio: ", image_ratio)
 
@@ -98,46 +111,29 @@ if __name__ == '__main__':
             img_blur = cv2.GaussianBlur(image_copy, (gw, gw), gs)
             g1 = cv2.GaussianBlur(img_blur, (gw1, gw1), gs1)
             g2 = cv2.GaussianBlur(img_blur, (gw2, gw2), gs2)
-            ret, thg = cv2.threshold(g2-g1, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            ret, thg = cv2.threshold(g2-g1, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
             cv2.imshow("image", thg)
 
             contours, hierarchy = cv2.findContours(thg, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-            res = 0
-
-            rects = [] 
-            end_x=0 
-            end_y=0
 
             print("len(contours): ", len(contours))
-
 
             for i in range(len(contours)):
 
                 if hierarchy[0][i][2] == -1:
                     continue
 
-
                 x ,y, w, h = cv2.boundingRect(contours[i]) 
-                aspectRatio = float(w)/h
-                if aspectRatio >= 1.5 and w> 0.33*width_image and aspectRatio <= 5:          
+                aspectRatio = float(w/h)
+                if aspectRatio >= 1.5 and w > 0.33*width_image and aspectRatio <= 8:          
                     approx = cv2.approxPolyDP(contours[i], 0.05* cv2.arcLength(contours[i], True), True)
-                    if len(approx) == 4:
-                        end_x=x+w
-                        end_y=y+h   
-                        rect = cv2.minAreaRect(contours[i])
-                        box = cv2.boxPoints(rect)
-                        box = np.intp(box)
-
+                    if len(approx) == 4: 
                         area = cv2.contourArea(contours[i])
-
-                        if area >= 15000 and area <=60000:
-                            # image = cv2.drawContours(image,[box],0,(0,255,255),1)
+                        if area >= 10000 and area <= 100000:
                             image = cv2.drawContours(image,[contours[i]],0,(255,255,0),2)
                             mask = cv2.drawContours(mask,[contours[i]],0,(255,255,0),2)
-                            # mask = cv2.drawContours(mask,[box],0,(255,255,255),1)
                             out[mask == 255] = image[mask == 255]
-                            cv2.putText(image, "rectangle "+str(x)+" , " +str(y-5), (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
                             # print("contours[i]: ", contours[i])
 
                             # Now crop
@@ -160,12 +156,10 @@ if __name__ == '__main__':
                             ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
                             criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
                             corners = cv2.cornerSubPix(out2,np.float32(centroids),(5,5),(-1,-1),criteria)
-                            # Threshold for an optimal value, it may vary depending on the image.
 
                             # Now draw them
                             corners = np.intp(corners)
                             image_show[corners[:,1],corners[:,0]] = [0,255,0]
-                            # print("corners: ", corners[:,:])
 
                             width_image_show = int(image_show.shape[1])
                             height_image_show = int(image_show.shape[0])
@@ -187,7 +181,6 @@ if __name__ == '__main__':
 
                             points = np.array([points_dict[0], points_dict[1], points_dict[2], points_dict[3]])
                             pts1 = np.float32([points[0], points[1], points[2], points[3]])
-                            # print("pts1: ", pts1)
                             pts2 = np.float32([[0,0],[width_image_show,0],[0,height_image_show],[width_image_show,height_image_show]])
                             M = cv2.getPerspectiveTransform(pts1,pts2)
                             dst = cv2.warpPerspective(image[topy:bottomy+1, topx:bottomx+1],M,(width_image_show,height_image_show))
@@ -210,24 +203,23 @@ if __name__ == '__main__':
                             th3 = cv2.cvtColor(th3, cv2.COLOR_GRAY2BGR)
 
                             for i in range(len(contours_plate)):
-                                # if hierarchy_plate[0][i][2] == -1:
-                                #     continue
+                                if hierarchy_plate[0][i][2] == -1:
+                                    continue
                                 x_plate ,y_plate, w_plate, h_plate = cv2.boundingRect(contours_plate[i])
 
                                 if h_plate > dst.shape[0]/3:
-                                # cv2.rectangle(th3, (x_plate, y_plate), (x_plate + w_plate_plate, y_plate + h_plate), (0,0,255), 2) 
-                                # th3 = cv2.drawContours(th3, [contours_plate[i]],0,(0,255,0),1)
                                     letter = dst[y_plate:y_plate+h_plate, x_plate:x_plate+w_plate].copy()
                                     # letter= cv2.copyMakeBorder(letter,5,5,5,5,cv2.BORDER_CONSTANT,value=black_padding)
 
                                     letter_copy = cv2.resize(letter, (32, 40), interpolation = cv2.INTER_AREA)
-                                    # letter_copy = cv2.cvtColor(letter_copy, cv2.COLOR_BGR2GRAY)
-                                    letter_copy = np.array(letter_copy) / 255.0
-                                    prediction = letters_recognition_model.predict(letter_copy[None,:,:], batch_size=1)
-                                    y_classes = prediction.argmax(axis=-1)
-                                    # print("prediction: ", letterLabels[y_classes])
+                                    letter_copy = np.array(letter_copy, dtype=np.float32) / 255.0
+                                    letter_copy = np.reshape(letter_copy, input_shape)
 
-                                    # print("letterDescription: ", letterDescription)
+                                    prediction = letters_recognition_model.set_tensor(input_details[0]['index'], letter_copy)
+                                    letters_recognition_model.invoke()
+                                    output_data = letters_recognition_model.get_tensor(output_details[0]['index'])
+                                    y_classes = np.argmax(output_data)
+
                                     finalLetters.append(''.join(letterLabels[y_classes]))
                                     cv2.putText(letter, str(letterLabels[y_classes]), (10, 10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
                                     cv2.imshow("image crop letter: " + str(i), letter)
